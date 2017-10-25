033d485ef8c85008763d77779bd67f4897461340
kenconow <wid.kencono@gmail.com>
2017-10-24 22:32:48 -0500

    added left menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { MenuModule, ContextMenuModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from "@angular/router";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+
+const appRoutes: Routes = [
+  { path: "", redirectTo: "/dashboard", pathMatch: "full" },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MenuModule,
+    ContextMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
